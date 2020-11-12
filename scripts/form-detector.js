7f2bf65130f210c8f42a60d0c5129742467a81a6
Juan Ramos <ptjuanramos@gmail.com>
2020-11-12 15:17:44 +0000

    On or off icon when form-data sim is activated or deactivated

--- scripts/form-detector.js
+++ scripts/form-detector.js
@@ -1,3 +1,26 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-    
+function initFormListener() {
+
+}
+
+function highlightForms() {
+
+}
+
+function enableFormDetection() {
+    chrome.storage.sync.get('isEnable', function(data) {
+        var current = data.isEnable;
+        var onOrOff = current ? 'on' : 'off';
+        chrome.browserAction.setIcon({path: 'icon-' + onOrOff + '-48.png'});
+        chrome.storage.sync.set({isEnable: !current});
+
+        if(current) {
+            highlightForms();
+        }
+    });
+}
+
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.storage.sync.set({isEnable: true});
 });
+
+chrome.browserAction.onClicked.addListener(enableFormDetection);
